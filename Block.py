572dd827588bcba51834aaa07e66313c6ae9bd65
Mustafa El-Antrawy <52972831+MustafaElAntrawy@users.noreply.github.com>
2021-05-31 18:39:30 +0200

    implement external_compaction method in MemoryManager class

---
+++ Block.py
@@ -0,0 +1,5 @@
+class Block:
+    def __init__(self, name: str, start_address: int, end_address: int):
+        self.name = name
+        self.start_address = start_address
+        self.end_address = end_address
--- MemoryManager.py
+++ MemoryManager.py
@@ -1,5 +1,6 @@
 from Process import Process
 from Hole import Hole
+from Block import Block
 from collections import namedtuple
 from typing import List
 
@@ -12,6 +13,8 @@ class MemoryManager:
         self.holes = list(map(lambda hole: Hole('Hole', hole[0], hole[0] + hole[1]), holes))
         self.old_processes = self._deduce_old_processes()
         self.new_processes = []
+        self.blocks = []
+        self.is_compacted = False
 
     def allocate_best_fit(self, new_process: Process) -> bool:
         return True
@@ -23,24 +26,43 @@ class MemoryManager:
         return True
 
     def get_memory_map(self) -> List[dict]:
-        for i, hole in enumerate(self.holes):
-            hole.name = f"Hole{i}"
-        segments = []
-        for process in self.new_processes:
-            segments += process.get_segments()
-
-        blocks = self.holes + self.old_processes + segments
-        blocks.sort(key=lambda block: block.start_address)
-
-        result = list(map(lambda block: {
+        if not self.is_compacted:
+            for i, hole in enumerate(self.holes):
+                hole.name = f"Hole{i}"
+            segments = []
+            for my_process in self.new_processes:
+                segments += my_process.get_segments()
+            self.blocks = self.holes + self.old_processes + segments
+        self.blocks.sort(key=lambda block: block.start_address)
+        return list(map(lambda block: {
             'name': block.name,
             'start': block.start_address,
             'end': block.end_address
-        }, blocks))
-
-        return result
+        }, self.blocks))
 
     def external_compaction(self):
+        self.blocks = []
+        segments = []
+        for my_process in self.new_processes:
+            segments += my_process.get_segments()
+        for segment in segments:
+            temp_block = Block(segment.name, segment.start_address, segment.end_address)
+            self.blocks.append(temp_block)
+        for my_process in self.old_processes:
+            temp_block = Block(my_process.name, my_process.start_address, my_process.end_address)
+            self.blocks.append(temp_block)
+
+        self.blocks.sort(key=lambda block: block.start_address)
+
+        for i, block in enumerate(self.blocks):
+            if i > 0 and i + 1 < len(self.blocks):
+                temp = self.blocks[i+1].start_address
+                self.blocks[i+1].start_address = block.end_address
+                self.blocks[i+1].end_address = self.blocks[i+1].start_address + (self.blocks[i+1].end_address - temp)
+        hole = Hole('Hole0', self.blocks[len(self.blocks)-1].end_address, self.total_memory_size)
+        self.holes = [hole]
+        self.blocks += self.holes
+        self.is_compacted = True
         pass
 
     def _deduce_old_processes(self) -> List[OldProcess]:
@@ -65,3 +87,58 @@ class MemoryManager:
             old_processes.append(OldProcess(f"Old{index}", last_hole.end_address, self.total_memory_size))
 
         return old_processes
+
+
+memory = MemoryManager(500, [[20, 40], [80, 80], [200, 200], [450, 30]])
+
+for process in memory.old_processes:
+    print(f"{process.start_address} => {process.end_address}")
+
+process = Process(1, [{
+        'name': 'code',
+        'size': 20,
+        'start_address': 20,
+        'end_address': 40
+    },
+     {
+         'name': 'Data',
+         'size': 80,
+         'start_address': 80,
+         'end_address': 160
+     }])
+process2 = Process(2, [{
+        'name': 'code',
+        'size': 20,
+        'start_address': 200,
+        'end_address': 220
+    },
+     {
+         'name': 'Data',
+         'size': 80,
+         'start_address': 240,
+         'end_address': 320
+     }])
+
+memory.new_processes.append(process)
+memory.new_processes.append(process2)
+
+memory.external_compaction()
+
+process3 = Process(3, [{
+        'name': 'code',
+        'size': 10,
+        'start_address': 450,
+        'end_address': 460
+    },
+     {
+         'name': 'Data',
+         'size': 10,
+         'start_address': 470,
+         'end_address': 480
+     }])
+
+memory.new_processes.append(process3)
+memory.external_compaction()
+my_list = memory.get_memory_map()
+
+print(my_list)
