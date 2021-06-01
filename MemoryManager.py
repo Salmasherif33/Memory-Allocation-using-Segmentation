0c7f48cc48ecf405d3363553f2b0223c64fad231
Mustafa El-Antrawy <52972831+MustafaElAntrawy@users.noreply.github.com>
2021-06-01 21:44:45 +0200

    implement get_new_processes and get_old_processes methods in MemoryManger class

--- MemoryManager.py
+++ MemoryManager.py
@@ -9,6 +9,7 @@ OldProcess = namedtuple('OldProcess', ['name', 'start_address', 'end_address'])
 def sorting(x):
     return(x[1])
 
+
 class MemoryManager:
     def __init__(self, total_memory_size, holes):
         self.total_memory_size = total_memory_size
@@ -17,151 +18,140 @@ class MemoryManager:
         self.new_processes = []
         self.blocks = []
         self.is_compacted = False
-        self.flag=0
-        self.holes_size =holes
-        self.output_holes=[]   
 
-    def allocate_best_fit(self, n_p) -> bool:
-         
-        self.holes_size.sort(key=sorting)
-        for i in self.holes_size:
-            cup =Hole("",i[0],i[1]+i[0])
-            self.output_holes.append(cup.str())
-        
-        for p_num in n_p.segments:
-        
-            h_num =[]
-            for h_num in self.holes_size:  
-                if p_num['size']<= h_num[1]:
-                    self.new_processes.append({'name':f"p{n_p.index}:{p_num['name']}",'size':p_num['size'],'start':h_num[0],'end':p_num['size']+h_num[0] })
-                  
-                    h_num[1]= h_num[1]-p_num['size']
-                    h_num[0]=p_num['size']+h_num[0]
-                    self.flag =0
-                    self.holes_size.sort(key=sorting)
+    def allocate_best_fit(self, new_process) -> bool:
+        flag = 0
+        segments = []
+        holes_size = []
+        for hole in self.holes:
+            holes_size.append([hole.start_address, hole.end_address - hole.start_address])
+
+        holes_size.sort(key=sorting)
+
+        for segment in new_process.segments:
+            h_num = []
+            for h_num in holes_size:
+                if segment['size'] <= h_num[1]:
+                    segments.append({
+                        'name': f"{segment['name']}",
+                        'size': segment['size'],
+                        'start_address': h_num[0],
+                        'end_address': segment['size'] + h_num[0]
+                    })
+
+                    h_num[1] = h_num[1] - segment['size']
+                    h_num[0] = segment['size'] + h_num[0]
+                    flag = 0
+                    holes_size.sort(key=sorting)
                     break
-                
-                if p_num['size']> h_num[1] :
-                    self.holes_size.sort(key=sorting)
-                    self.flag =1
-            
-            if self.flag ==1:
-              
-                self.new_processes.clear()
-                break         
-        
-    
-        if self.flag == 1:
-            self.holes = self.output_holes 
-            #print (self.new_processes)
-            #print (self.output_holes)
-            return False 
-        self.output_holes.clear()
-        for i in self.holes_size:
-            cup =Hole("",i[0],i[1]+i[0])
-            self.output_holes.append(cup.str())
-        self.holes = self.output_holes    
-        #print (self.new_processes)
-        #print (self.holes)
 
-       
+                if segment['size'] > h_num[1]:
+                    holes_size.sort(key=sorting)
+                    flag = 1
+
+            if flag == 1:
+                break
+
+        if flag == 1:
+            return False
+        self.new_processes.append(Process(len(self.new_processes), segments))
+        self.holes.clear()
+        for i in holes_size:
+            cup = Hole("", i[0], i[1] + i[0])
+            if cup.start_address != cup.end_address:
+                self.holes.append(cup)
         return True
 
-    def allocate_worst_fit(self,  n_p) -> bool:
-        #print(self.holes)
-        self.holes_size.sort(key=sorting,reverse = True)
-        for i in self.holes_size:
-            cup =Hole("",i[0],i[1]+i[0])
-            self.output_holes.append(cup.str())
+    def allocate_worst_fit(self, new_process) -> bool:
+        flag = 0
+        segments = []
+        holes_size = []
+        for hole in self.holes:
+            holes_size.append([hole.start_address, hole.end_address - hole.start_address])
+
+        holes_size.sort(key=sorting, reverse=True)
         
-        for p_num in n_p.segments:
+        for segment in new_process.segments:
         
-            h_num =[]
-            for h_num in self.holes_size:  
-                if p_num['size']<= h_num[1]:
-                    self.new_processes.append({'name':f"p{n_p.index}:{p_num['name']}",'size':p_num['size'],'start':h_num[0],'end':p_num['size']+h_num[0] })
+            h_num = []
+            for h_num in holes_size:
+                if segment['size'] <= h_num[1]:
+                    segments.append(
+                        {'name': f"{segment['name']}",
+                         'size': segment['size'],
+                         'start_address': h_num[0],
+                         'end_address': segment['size'] + h_num[0]})
                   
-                    h_num[1]= h_num[1]-p_num['size']
-                    h_num[0]=p_num['size']+h_num[0]
-                    self.flag =0
-                    self.holes_size.sort(key=sorting,reverse = True)
+                    h_num[1] = h_num[1]-segment['size']
+                    h_num[0] = segment['size']+h_num[0]
+                    flag = 0
+                    holes_size.sort(key=sorting, reverse=True)
                     break
                 
-                if p_num['size']> h_num[1] :
-                    self.holes_size.sort(key=sorting,reverse = True)
-                    self.flag =1
+                if segment['size'] > h_num[1]:
+                    holes_size.sort(key=sorting, reverse=True)
+                    flag = 1
             
-            if self.flag ==1:
-              
-                self.new_processes.clear()
+            if flag == 1:
                 break         
-        
-       
 
-        if self.flag == 1:
-            #print (self.new_processes)
-            #print (self.output_holes)
-            self.holes = self.output_holes 
-            return False 
-        self.output_holes.clear()
-        for i in self.holes_size:
-            cup =Hole("",i[0],i[1]+i[0])
-            self.output_holes.append(cup.str())
-        self.holes = self.output_holes 
-        #print (self.new_processes)
-        #print (self.holes)
+        if flag == 1:
+            return False
+        self.new_processes.append(Process(len(self.new_processes), segments))
+        self.holes.clear()
+        for i in holes_size:
+            cup = Hole("", i[0], i[1]+i[0])
+            if cup.start_address != cup.end_address:
+                self.holes.append(cup)
 
         return True
 
-    def allocate_first_fit(self, n_p) -> bool:
-        for i in self.holes_size:
-            cup =Hole("",i[0],i[1]+i[0])
-            self.output_holes.append(cup.str())
+    def allocate_first_fit(self, new_process) -> bool:
+        flag = 0
+        segments = []
+        holes_size = []
+
+        for hole in self.holes:
+            holes_size.append([hole.start_address, hole.end_address - hole.start_address])
         
-        for p_num in n_p.segments:
+        for segment in new_process.segments:
         
-            h_num =[]
-            for h_num in self.holes_size:  
-                if p_num['size']<= h_num[1]:
-                    self.new_processes.append({'name':f"p{n_p.index}:{p_num['name']}",'size':p_num['size'],'start':h_num[0],'end':p_num['size']+h_num[0] })
-                  
-                    h_num[1]= h_num[1]-p_num['size']
-                    h_num[0]=p_num['size']+h_num[0]
-                    self.flag =0
-                    
+            h_num = []
+            for h_num in holes_size:
+                if segment['size'] <= h_num[1]:
+                    segments.append({
+                        'name': f"{segment['name']}",
+                        'size': segment['size'],
+                        'start_address': h_num[0],
+                        'end_address': segment['size'] + h_num[0]
+                    })
+
+                    h_num[1] = h_num[1]-segment['size']
+                    h_num[0] = segment['size']+h_num[0]
+                    flag = 0
                     break
                 
-                if p_num['size']> h_num[1] :
-                    
-                    self.flag =1
+                if segment['size'] > h_num[1]:
+                    flag = 1
             
-            if self.flag ==1:
-              
-                self.new_processes.clear()
+            if flag == 1:
                 break         
-        
-       
 
-        if self.flag == 1:
-            #print (self.new_processes)
-            #print (self.output_holes)
-            self.holes = self.output_holes 
-            return False 
+        if flag == 1:
+            return False
 
-        self.output_holes.clear()
-        for i in self.holes_size:
-            cup =Hole("",i[0],i[1]+i[0])
-            self.output_holes.append(cup.str())
-        self.holes = self.output_holes   
-        #print (self.new_processes)
-        #print (self.holes)
-        
-
+        self.new_processes.append(Process(len(self.new_processes), segments))
+        self.holes.clear()
+        for i in holes_size:
+            cup = Hole("", i[0], i[1]+i[0])
+            if cup.start_address != cup.end_address:
+                self.holes.append(cup)
 
         return True
 
     def get_memory_map(self) -> List[dict]:
         if not self.is_compacted:
+            self.holes.sort(key=lambda hole: hole.start_address)
             for i, hole in enumerate(self.holes):
                 hole.name = f"Hole{i}"
             segments = []
@@ -199,6 +189,30 @@ class MemoryManager:
         self.blocks += self.holes
         self.is_compacted = True
         pass
+
+    def get_new_processes(self):
+        return list(map(lambda my_process: {
+            'name': f"P{my_process.index}",
+            'number of segments': len(my_process.segments),
+        }, self.new_processes))
+
+    def get_old_processes(self):
+        if self.is_compacted:
+            result = []
+            for block in self.blocks:
+                if block.name[0] == "O":
+                    result.append({
+                        'name': block.name,
+                        'start address': block.start_address,
+                        'size': block.end_address - block.start_address
+                    })
+            return result
+
+        return list(map(lambda my_process: {
+            'name': my_process.name,
+            'start address': my_process.start_address,
+            'size': my_process.end_address - my_process.start_address
+        }, self.old_processes))
 
     def _deduce_old_processes(self) -> List[OldProcess]:
         # handle corner case when there are no holes
@@ -274,17 +288,20 @@ process3 = Process(3, [{
 
 memory.new_processes.append(process3)
 memory.external_compaction()
-my_list = memory.get_memory_map()
+my_list = memory.get_old_processes()
 
 print(my_list)
 print('testing the algorithm')
 
-ayaa=Process(1,[{'name':"code", 'size': 50},{'name':"data", 'size': 300},{'name':"stack", 'size': 100},{'name':"stack", 'size': 100}])
+ayaa = Process(1, [{'name': "code", 'size': 50}, {'name': "data", 'size': 300}, {'name': "stack", 'size': 100}, {'name': "stack", 'size': 100}])
 
 #p = Process(1,[{'name':"code", 'size': '100'},{'name':"data", 'size': '250'}])
 #s=([0,200],[300,500],[900,100])
-s=[[0,200],[300,400],[900,60]]
-aya = MemoryManager(1000,s)
 
-a=aya.allocate_best_fit( ayaa)
-print(a)
+s = [[0, 200], [300, 400], [900, 60]]
+aya = MemoryManager(1000, s)
+
+a = aya.allocate_worst_fit(ayaa)
+antrawy = aya.get_memory_map()
+print(antrawy)
+
